e3395bfd23af00f434341e7bfad2db484260cd29
Lyunin <95631880+EastsCloud@users.noreply.github.com>
2026-04-28 12:29:05 -0400

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        p = [[] for i in range(numCourses)]
+        cy = True
+        for i in prerequisites:
+            p[i[0]].append(i[1])
+        print(p)
+        vis = [0 for i in range(numCourses)]
+        def dfs(c) -> None:
+            nonlocal cy
+            vis[c] = 1
+            for i in p[c]:
+                if vis[i] == 0:
+                    dfs(i)
+                else:
+                    cy = False
+                    return
+            vis[c] = 0
+            return
+        
+        for i in range(numCourses):
+            if vis[i] == 0:
+                dfs(i)
+
+        return cy
+        
